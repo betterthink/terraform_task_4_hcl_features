29caa3de0348aa55fd1e3e77b28a1992b93746d3
betterthink <mykyta.kovalenko0804@gmail.com>
2026-02-11 14:49:33 +0100

    fixed typo

--- network.tf
+++ network.tf
@@ -29,7 +29,7 @@ resource "azurerm_network_security_group" "example" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
-  dynamic "security_rules" {
+  dynamic "security_rule" {
     for_each = local.security_rules
     content {
       name                       = security_rule.value.name
